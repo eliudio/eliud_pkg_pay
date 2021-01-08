76fd589a2e0aa1f0a25dae4f8ae02a5eac11f3cb
Eliud Berlin <info@eliud.io>
2021-01-08 23:07:48 +0100

    Membership Pre test

--- lib/pay_package.dart
+++ lib/pay_package.dart
@@ -10,6 +10,7 @@ import 'package:eliud_pkg_pay/tools/task/review_and_ship_task_entity.dart';
 import 'package:eliud_pkg_pay/tools/task/review_and_ship_task_model.dart';
 import 'package:eliud_pkg_workflow/tools/task/task_model.dart';
 import 'package:flutter_bloc/src/bloc_provider.dart';
+import 'package:eliud_core/model/access_model.dart';
 
 abstract class PayPackage extends Package {
   @override
@@ -17,7 +18,7 @@ abstract class PayPackage extends Package {
   }
 
   @override
-  Future<bool> isConditionOk(String packageCondition, AppModel app, MemberModel member, bool isOwner, int privilegeLevel) => null;
+  Future<bool> isConditionOk(String packageCondition, AppModel app, MemberModel member, bool isOwner, bool isBlocked, PrivilegeLevel privilegeLevel) => null;
 
   @override
   List<String> retrieveAllPackageConditions() => null;
