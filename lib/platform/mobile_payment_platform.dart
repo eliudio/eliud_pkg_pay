ac5c458b66907e8430001c7c1e23367690aed08b
Eliud <58745656+eliudio@users.noreply.github.com>
2022-09-21 13:32:06 +0100

    removed stripe

--- lib/platform/mobile_payment_platform.dart
+++ lib/platform/mobile_payment_platform.dart
@@ -2,97 +2,21 @@ import 'dart:convert';
 
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:eliud_core/model/app_model.dart';
+import 'package:eliud_core/style/frontend/has_dialog.dart';
 import 'package:eliud_core/style/style_registry.dart';
 import 'payment_platform.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:stripe_payment/stripe_payment.dart';
 
 class MobilePaymentPlatform extends AbstractPaymentPlatform {
   final bool? requiresConfirmation;
 
   MobilePaymentPlatform({this.requiresConfirmation});
 
-  final HttpsCallable INTENT = FirebaseFunctions.instance.httpsCallable('createPaymentIntent');
-
   @override
-  void startPaymentProcess(AppModel app, BuildContext theContext, HandlePayment handlePayment, String? name, String? ccy, double? amount) {
-    var cents = (amount! * 100).toInt();
-    StripePayment.setOptions(StripeOptions(
-        publishableKey: 'pk_test_51GxyTUCM4yYbbMk8IvFWz65Lp5aCXN16iZcQsr0Z1VYQPlrwe7GhoJ4ZTdF571VYTzOrzHAvr0Q5LKdTCZ3naaYo00T4RFwif7',
-        androidPayMode: 'test'));
-
-    var prefilledInformation  = PrefilledInformation(
-        billingAddress: BillingAddress(
-        ));
-    var options = CardFormPaymentRequest(requiredBillingAddressFields: 'full', prefilledInformation: prefilledInformation);
-
-    StripePayment.paymentRequestWithCardForm(options)
-        .then((paymentMethod) {
-      var amount = cents.toDouble();
-      INTENT.call(<String, dynamic>{'amount': amount,'currency':ccy}).then((response) {
-        confirmDialog(app, theContext, response.data['client_secret'], paymentMethod, handlePayment, ccy, amount); //function for confirmation for payment
-      });
-    });
-  }
-
-  void confirmDialog(AppModel app, BuildContext context, String? clientSecret, PaymentMethod paymentMethod, HandlePayment handlePayment, String? ccy, double amount) {
-    if ((requiresConfirmation != null) && requiresConfirmation!) {
-      var confirm = AlertDialog(
-        title: Text('Confirm Payment'),
-        content: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                'Make Payment',
-                // style: TextStyle(fontSize: 25),
-              ),
-              Text('Charge amount:' + amount.toString() + " " + ccy!)
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          StyleRegistry.registry().styleWithApp(app).frontEndStyle().buttonStyle().button(app, context, label: 'Cancel',
-            onPressed: () {
-              Navigator.of(context).pop();
-              final snackBar = SnackBar(content: Text('Payment Cancelled'),);
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            },
-          ),
-          StyleRegistry.registry().styleWithApp(app).frontEndStyle().buttonStyle().button(app, context, label: 'Confirm',
-            onPressed: () {
-              Navigator.of(context).pop();
-              confirmPayment(clientSecret, paymentMethod,
-                handlePayment,); // function to confirm Payment
-            },
-          ),
-        ],
-      );
-      showDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (BuildContext context) {
-            return confirm;
-          });
-    } else {
-      confirmPayment(clientSecret, paymentMethod,
-        handlePayment,); // function to confirm Payment
-    }
-  }
-
-  void confirmPayment(String? sec, PaymentMethod paymentMethod, HandlePayment handlePayment) {
-    StripePayment.confirmPaymentIntent(
-      PaymentIntent(clientSecret: sec, paymentMethodId: paymentMethod.id),
-    ).then((val) {
-      if (val.status == 'succeeded') {
-        handlePayment(PaymentSucceeded(val.paymentIntentId));
-      } else {
-        handlePayment(PaymentFailure(val.status, jsonEncode(val.toJson())));
-      }
-    }).catchError((onError) {
-      handlePayment(PaymentFailure(onError, ''));
-    });
+  void startPaymentProcess(AppModel app, BuildContext context, HandlePayment handlePayment, String? name, String? ccy, double? amount) {
+    openErrorDialog(app, context, app.documentID + '/_paymenterror',
+        title: 'Error',
+        errorMessage: 'Stripe payment not implemented (existing stripe package not supported anymore)');
   }
 }
--- lib/platform/web_payment_platform.dart
+++ lib/platform/web_payment_platform.dart
@@ -1,29 +1,15 @@
 import 'package:eliud_core/model/app_model.dart';
+import 'package:eliud_core/style/frontend/has_dialog.dart';
 import 'package:eliud_pkg_pay/platform/payment_platform.dart';
-import 'package:eliud_pkg_pay/platform/web_helper/web_stripe_elements.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WebPaymentPlatform extends AbstractPaymentPlatform {
   @override
   void startPaymentProcess(AppModel app,
-      BuildContext? context, HandlePayment handlePayment, String? name, String? ccy, double? amount) {
-    debugPrint("startPaymentProcess");
-    var cents = (amount! * 100).toInt();
-    debugPrint("before showDialog");
-    showDialog(
-        context: context!,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: Container(
-                width: 400.0,
-                height: 100.0,
-                child: WebStripeElements(
-                    ccy: ccy,
-                    name: name,
-                    cents: cents,
-                    handlePayment: handlePayment)),
-          );
-        });
+      BuildContext context, HandlePayment handlePayment, String? name, String? ccy, double? amount) {
+    openErrorDialog(app, context, app.documentID + '/_paymenterror',
+        title: 'Error',
+        errorMessage: 'Stripe payment not implemented yet (existing stripe package not supported anymore)');
   }
 }
